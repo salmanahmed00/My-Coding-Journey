baeba5fd0a1a02b0d444b9ffcc45cd4a3766483d
Salman Ahmed <ahmedpec1005@gmail.com>
2022-06-26 11:27:51 +0530

    Update problem2.cpp

--- Recursion/problem2.cpp
+++ Recursion/problem2.cpp
@@ -30,6 +30,26 @@ void MinMax(vector<int>& arr, int ind, int n, int& min, int& max){
     MinMax(arr, ind+1, n, min, max);
 }
 
+
+// Method 2
+
+int Min(vector<int>& arr, int n){
+    if(n == 1){
+        return arr[n-1];
+    }
+
+    return min(arr[n-1], Min(arr, n-1));
+}
+
+int Max(vector<int>& arr, int n){
+    if(n == 1){
+        return arr[n-1];
+    }
+
+    return max(arr[n-1], Max(arr, n-1));
+}
+
+
 int main(){
 
     vector<int> arr{33,5,2,34,51,1,45};
@@ -40,5 +60,7 @@ int main(){
     MinMax(arr, 0, arr.size(), min, max);
 
     cout<<"Minimum element is: "<<min<<" and Maximum element is: "<<max<<endl;
+    
+    cout<<"Minimum element is: "<<Min(arr, arr.size())<<" and Maximum element is: "<<Max(arr, arr.size())<<endl;
 
 }
